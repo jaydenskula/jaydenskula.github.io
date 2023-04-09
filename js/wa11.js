0cdedb57c76c4709065b87dc29db7316dd0d3446
Jayden Skuladottir <sask5403@colorado.edu>
2023-04-09 13:01:55 -0600

    Update wa11.js

--- js/wa11.js
+++ js/wa11.js
@@ -1,13 +1,40 @@
-const imgAPI = "https://random.dog/";
-const quoteAPI = "https://type.fit/api/quotes"
+const imgAPI = "https://random.dog/woof.json";
+const quoteAPI = "https://api.quotable.io/random";
 
+const button = document.querySelector(".button");
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+
+let c = 0;
+
+function changeQuote()
+{
+	fetch(quoteAPI)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
+}
 
 function changeImage()
 {
 	fetch(imgAPI)
-	.then(response => response.changeImage())
-	.then(data => console.log(data));
+		.then(response => response.text())
+		.then(data => {console.log(data)})
+		.catch(error => console.error(error))
 }
 
-const button = document.querySelector(".button");
+function quotesGenerated()
+{
+
+}
+
 button.addEventListener("click", changeImage);
+button.addEventListener("click", changeQuote);
+button.addEventListener('click', () => {
+    c++; 
+    count.innerText = `You've generated ${c} quotes!`;
+})
+
+changeQuote(changeQuote);
