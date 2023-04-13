54a49838dd901a3df24f42dcff9b35f81aa914b7
Jayden Skuladottir <sask5403@colorado.edu>
2023-04-12 20:43:56 -0600

    Update ica12.js

--- js/ica12.js
+++ js/ica12.js
@@ -75,15 +75,15 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 25) {
-   const size = random(10,20);
+while (balls.length < 100) {
+   const size = random(5,30);
    const ball = new Ball(
       // ball position always drawn at least one ball width
       // away from the edge of the canvas, to avoid drawing errors
       random(0 + size,width - size),
       random(0 + size,height - size),
-      random(-7,7),
-      random(-7,7),
+      random(-7,5),
+      random(-10,10),
       randomRGB(),
       size
    );
