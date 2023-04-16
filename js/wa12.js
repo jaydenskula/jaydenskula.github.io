6f2285b2fd8fe4594784445f725e7b09b29e3e1c
Jayden Skuladottir <sask5403@colorado.edu>
2023-04-16 11:07:02 -0600

    Update wa12.js

---
+++ js/wa12.js
@@ -0,0 +1,54 @@
+// PROBLEM #1
+// Employee 1
+let employee1 = 
+{
+    "first_name": "Sam",
+    "department": "Tech",
+    "designation": "Manager",
+    "salary": 40000,
+    "raise_eligible": true
+}
+  
+// Employee 2
+let employee2 = 
+{
+    "first_name": "Mary",
+    "department": "Finance",
+    "designation": "Trainee",
+    "salary": 18500,
+    "raise_eligible": true
+};
+  
+// Employee 3
+let employee3 = 
+{
+    "first_name": "Bill",
+    "department": "HR",
+    "designation": "Executive",
+    "salary": 21200,
+    "raise_eligible": false
+};
+
+console.log(employee1);
+console.log(employee2);
+console.log(employee3);
+
+
+// PROBLEM #2
+let company =
+{
+    "companyName": "Tech Stars",
+    "website": "www.techstars.site",
+    "employees": [employee1, employee2, employee3]
+}
+
+console.log(company);
+
+//PROBLEM #3
+
+//PROBLEM #4
+
+//PROBLEM #5
+
+//PROBLEM #6
+  
