83c7eff023b8456ee2496c53aeb90fd639ea1ce9
Jayden Skuladottir <sask5403@colorado.edu>
2023-04-23 11:28:18 -0600

    Update wa13.js

--- js/wa13.js
+++ js/wa13.js
@@ -1,51 +1,119 @@
-function check() {
-    console.log('test');
-}
+var containerWidth = document.body.clientWidth;
+var containerHeight = document.body.clientHeight;
 
-function submit() {
-    alert('Your volume is now: ' + output.textContent);
-}
+// function check() 
+// {
+//     console.log('test');
+// }
 
-function reset() {
+// function submit() 
+// {
+//     alert('Your volume is now: ' + output.textContent);
+// }
+
+function reset() 
+{
     outputInt = 0;
-    output.textContent = outputInt;
+    output.textContent = outputInt.toFixed(2);
+
+    var resetButtonWidth = resetButton.offsetWidth;
+    var resetButtonHeight = resetButton.offsetHeight;
+  
+    // Calculate a random position for the element
+    var randomX = Math.floor(Math.random() * (containerWidth - resetButtonWidth));
+    var randomY = Math.floor(Math.random() * (containerHeight - resetButtonHeight));
+  
+    // Set the new position of the element
+    resetButton.style.position = "absolute";
+    resetButton.style.left = randomX + "px";
+    resetButton.style.top = randomY + "px";
 }
 
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
-    
-}
-
-function plus() {
-    if (outputInt < 100) {
-    outputInt +=1;
-    output.textContent = outputInt;
+function minus() 
+{
+    if (outputInt > 0) 
+    {
+    outputInt -=0.1;
+    output.textContent = outputInt.toFixed(2); 
     }
+    
+    var minusButtonWidth = minusButton.offsetWidth;
+    var minusButtonHeight = minusButton.offsetHeight;
+  
+    // Calculate a random position for the element
+    var randomX = Math.floor(Math.random() * (containerWidth - minusButtonWidth));
+    var randomY = Math.floor(Math.random() * (containerHeight - minusButtonHeight));
+  
+    // Set the new position of the element
+    minusButton.style.position = "absolute";
+    minusButton.style.left = randomX + "px";
+    minusButton.style.top = randomY + "px";
 }
 
-function random() {
+function plus() 
+{
+    if (outputInt < 100) 
+    {
+    outputInt += 0.1;
+    output.textContent = outputInt.toFixed(2);
+    }
+
+    var plusButtonWidth = plusButton.offsetWidth;
+    var plusButtonHeight = plusButton.offsetHeight;
+  
+    // Calculate a random position for the element
+    var randomX = Math.floor(Math.random() * (containerWidth - plusButtonWidth));
+    var randomY = Math.floor(Math.random() * (containerHeight - plusButtonHeight));
+  
+    // Set the new position of the element
+    plusButton.style.position = "absolute";
+    plusButton.style.left = randomX + "px";
+    plusButton.style.top = randomY + "px";
+}
+
+function random() 
+{
     outputInt = randomNumber(0, 100);
-    output.textContent = outputInt;
+    output.textContent = outputInt.toFixed(2);
+
+    var randomButtonWidth = randomButton.offsetWidth;
+    var randomButtonHeight = randomButton.offsetHeight;
+  
+    // Calculate a random position for the element
+    var randomX = Math.floor(Math.random() * (containerWidth - randomButtonWidth));
+    var randomY = Math.floor(Math.random() * (containerHeight - randomButtonHeight));
+  
+    // Set the new position of the element
+    randomButton.style.position = "absolute";
+    randomButton.style.left = randomX + "px";
+    randomButton.style.top = randomY + "px";
 }
 
-function randomNumber(min, max) {
+function randomNumber(min, max) 
+{
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
     return num;
-  }
-
-
+}
 
 const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
 console.log(outputInt);
 
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+const minusButton = document.querySelector('.minus-button');
+minusButton.addEventListener('click', minus);
+
+const plusButton = document.querySelector('.plus-button');
+plusButton.addEventListener('click', plus);
+
+const resetButton = document.querySelector('.reset-button');
+resetButton.addEventListener('click', reset);
+
+const randomButton = document.querySelector('.random-button');
+randomButton.addEventListener('click', random);
+
+// const submitButton = document.querySelector('.submit-button');
+// submitButton.addEventListener('click', submit);
+
 
 
 /* const button = document.querySelector('.button');
@@ -60,13 +128,12 @@ function updateOutput() {
 }
 */
 
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
+// var slider = document.getElementById("myRange");
+// var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
+// var sliderOutput = document.querySelector(".slider-output");
 
 
 // Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
-}
+// function update() {
+//   sliderOutput.textContent = slider.value;
+// }
